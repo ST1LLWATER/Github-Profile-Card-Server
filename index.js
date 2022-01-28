49281d70c4859366f94587c0b94d13b78f2d8402
st1llwater <alokpunjofficial@gmail.com>
2022-01-29 01:58:44 +0530

    CORS Fixed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 const PORT = process.env.PORT || 4000;
 

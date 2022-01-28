f5785077c4451edf79cd87fe1d12f379ece2fde9
N-Deepika <deepika282002@gmail.com>
2022-01-28 19:49:35 +0530

    Structural changes.

--- index.js
+++ index.js
@@ -7,25 +7,22 @@ app.use(express.json());
 
 app.get('/api/stats/:username', async (req, res) => {
   const { username } = req.params;
-  let totalRepos = 0,
-      totalFollowers = 0,
-      totalFollowing = 0,
-      totalStars = 0;
-  let curRepos = 100,
-      curFollowers = 100,
-      curFollowing = 100,
-      curStars = 100;
+  let totalStars=0;
+  let curStars=100;
   let value = 1,
       page = 1;
 
-  console.log({ curRepos, curFollowers, curFollowing, curStars });
 
   const { data } = await axios.get(
       `https://api.github.com/search/commits?q=author:${username}`
   );
 
+  const userData=await axios.get(
+      `https://api.github.com/users/${username}`
+  ).then(res=>res.data);
+
   while (value > 0) {
-    if(curStars === 100){
+    if(curStars == 100){
       const stars = await axios.get(
           `https://api.github.com/users/${username}/repos`,
           {
@@ -36,67 +33,21 @@ app.get('/api/stats/:username', async (req, res) => {
         totalStars+=val.stargazers_count;
       })
       curStars = stars.data.length;
-    }
-    else{
-      curStars=0
-    }
-
-    if (curFollowers === 100) {
-      const followers = await axios.get(
-          `https://api.github.com/users/${username}/followers`,
-          {
-            params: { page ,per_page:100},
-          }
-      );
-      curFollowers = followers.data.length;
-    }
-    else{
-      curFollowers=0
+    }else{
+      curStars=0;
     }
 
-
-    if (curFollowing === 100) {
-      const following = await axios.get(
-          `https://api.github.com/users/${username}/following`,
-          {
-            params: { page , per_page:100},
-          }
-      );
-      curFollowing = following.data.length;
-    }
-    else{
-      curFollowing=0
-    }
-
-
-    if (curRepos === 100) {
-      const repos = await axios.get(
-          `https://api.github.com/users/${username}/repos`,
-          {
-            params: { page, per_page:100 },
-          }
-      );
-      curRepos = repos.data.length;
-    }
-    else{
-      curRepos=0
-    }
-
-
-    totalFollowers += curFollowers;
-    totalFollowing += curFollowing;
-    totalRepos += curRepos;
     page += 1;
-    console.log({ curStars, curFollowers, curFollowing, curRepos });
-    value = curStars || curFollowers || curFollowing || curRepos;
-    console.log(value);
+    value = curStars;
   }
   res.json({
+    username:userData.login,
+    avatar:userData.avatar_url,
     commits: data.total_count,
-    stars: totalStars,
-    followers: totalFollowers,
-    following: totalFollowing,
-    repos: totalRepos,
+    stars:totalStars,
+    followers: userData.followers,
+    following: userData.following,
+    repos: userData.public_repos,
   });
 });
 

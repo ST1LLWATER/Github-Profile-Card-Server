27179a5d62d5114b0879f9be16306f9f3d08ad78
N-Deepika <deepika282002@gmail.com>
2022-01-28 19:30:05 +0530

    Optimized

--- index.js
+++ index.js
@@ -8,86 +8,92 @@ app.use(express.json());
 app.get('/api/stats/:username', async (req, res) => {
   const { username } = req.params;
   let totalRepos = 0,
-    totalFollowers = 0,
-    totalFollowing = 0,
-    totalStars=0;
+      totalFollowers = 0,
+      totalFollowing = 0,
+      totalStars = 0;
   let curRepos = 100,
-    curFollowers = 100,
-    curFollowing = 100,
-    curStars=100;
+      curFollowers = 100,
+      curFollowing = 100,
+      curStars = 100;
   let value = 1,
-    page = 1;
+      page = 1;
 
+  console.log({ curRepos, curFollowers, curFollowing, curStars });
 
   const { data } = await axios.get(
-    `https://api.github.com/search/commits?q=author:${username}`
+      `https://api.github.com/search/commits?q=author:${username}`
   );
 
   while (value > 0) {
-    if(curStars == 100){
-       const stars = await axios.get(
-        `https://api.github.com/users/${username}/repos`,
-        {
-          params: { page,per_page:100 },
-        }
+    if(curStars === 100){
+      const stars = await axios.get(
+          `https://api.github.com/users/${username}/repos`,
+          {
+            params: { page,per_page:100 },
+          }
       );
       stars.data.map(val=>{
-      totalStars+=val.stargazers_count;
-    })
+        totalStars+=val.stargazers_count;
+      })
       curStars = stars.data.length;
-    }else{
-      curStars=0;
+    }
+    else{
+      curStars=0
     }
 
-    if (curFollowers == 100) {
+    if (curFollowers === 100) {
       const followers = await axios.get(
-        `https://api.github.com/users/${username}/followers`,
-        {
-          params: { page,per_page:100 },
-        }
+          `https://api.github.com/users/${username}/followers`,
+          {
+            params: { page ,per_page:100},
+          }
       );
-    
       curFollowers = followers.data.length;
-    }else{
-      curFollowers=0;
+    }
+    else{
+      curFollowers=0
     }
 
-    if (curFollowing == 100) {
+
+    if (curFollowing === 100) {
       const following = await axios.get(
-        `https://api.github.com/users/${username}/following`,
-        {
-          params: { page,per_page:100 },
-        }
+          `https://api.github.com/users/${username}/following`,
+          {
+            params: { page , per_page:100},
+          }
       );
       curFollowing = following.data.length;
-    }else{
-      curFollowing=0;
+    }
+    else{
+      curFollowing=0
     }
 
-    if (curRepos == 100) {
+
+    if (curRepos === 100) {
       const repos = await axios.get(
-        `https://api.github.com/users/${username}/repos`,
-        {
-          params: { page,per_page:100 },
-        }
+          `https://api.github.com/users/${username}/repos`,
+          {
+            params: { page, per_page:100 },
+          }
       );
       curRepos = repos.data.length;
-    }else{
-      curRepos=0;
+    }
+    else{
+      curRepos=0
     }
 
-   
+
     totalFollowers += curFollowers;
     totalFollowing += curFollowing;
     totalRepos += curRepos;
     page += 1;
-    console.log({ curFollowers, curFollowing, curRepos, curStars });
-    value = curFollowers || curFollowing || curRepos || curStars;
+    console.log({ curStars, curFollowers, curFollowing, curRepos });
+    value = curStars || curFollowers || curFollowing || curRepos;
     console.log(value);
   }
   res.json({
     commits: data.total_count,
-    stars:totalStars,
+    stars: totalStars,
     followers: totalFollowers,
     following: totalFollowing,
     repos: totalRepos,
